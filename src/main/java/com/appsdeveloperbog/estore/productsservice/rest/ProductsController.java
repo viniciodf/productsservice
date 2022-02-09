8016cbf8d1b8fd26480ba139baa2a7a404e2e5a4
vinicio ferreira <vinicio.ferreira@cvortex.io>
2022-02-09 07:44:22 -0300

    Initial commit

---
+++ src/main/java/com/appsdeveloperbog/estore/productsservice/rest/ProductsController.java
@@ -0,0 +1,9 @@
+package com.appsdeveloperbog.estore.productsservice.rest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductsController {
+}
---
+++ src/test/java/com/appsdeveloperbog/estore/productsservice/ProductsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperbog.estore.productsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
