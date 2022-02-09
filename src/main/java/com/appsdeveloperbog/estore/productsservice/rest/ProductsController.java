b40ffc041e32f63bdfe07441d98d158ec2dc38c3
vinicio ferreira <vinicio.ferreira@cvortex.io>
2022-02-09 07:54:19 -0300

    added restrcontroller

--- src/main/java/com/appsdeveloperbog/estore/productsservice/rest/ProductsController.java
+++ src/main/java/com/appsdeveloperbog/estore/productsservice/rest/ProductsController.java
@@ -1,9 +1,28 @@
 package com.appsdeveloperbog.estore.productsservice.rest;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/products")
 public class ProductsController {
+
+    @PostMapping
+    public String createProduct(){
+        return "HTTP POST Handled";
+    }
+
+    @GetMapping
+    public String getProduct(){
+        return "HTTP GET Handled";
+    }
+
+    @PutMapping
+    public String updateProduct(){
+        return "HTTP PUT Handled";
+    }
+
+    @DeleteMapping
+    public String deleteProduct(){
+        return "HTTP DELETE Handled";
+    }
 }
